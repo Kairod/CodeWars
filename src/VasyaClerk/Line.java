f23a50b06747539b79552e10efd10d7cd82d766b
Kairod <nicolas.kerkar@gmail.com>
2019-12-06 15:42:10 +0100

    Add some comments

--- src/VasyaClerk/Line.java
+++ src/VasyaClerk/Line.java
@@ -1,8 +1,12 @@
 package VasyaClerk;
 
+/*
+I need to change the money in three separated values: billet of 25, 50 and 100
+ */
+
 public class Line {
     public static String Tickets(int[] peopleInLine){
-        int money[] = {0, 0, 0};
+        int money = 0;
         final int price = 25;
         int diff = 0;
         for (Integer client : peopleInLine) {
